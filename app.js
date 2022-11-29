c9e622bba00a8cdeb02906d1b10369ed45c38d36
orlando.tyrell <orlandotyrell@gmail.com>
2022-11-29 14:54:58 -0500

    added Cors

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require("cors");
 require("dotenv").config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var projectRouter = require('./routes/project')
 var { mongoConnect } = require('./mongo.js');
 mongoConnect();
 
 var app = express();
+app.use(cors());
+app.options("*", cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/project', projectRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
